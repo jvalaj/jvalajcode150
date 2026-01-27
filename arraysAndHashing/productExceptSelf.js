a5769d27ea340e453cc891fa2437b9f0148c7701
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2026-01-27 11:26:40 -0500

    6

---
+++ arraysAndHashing/productExceptSelf.js
@@ -0,0 +1,34 @@
+
+/*intution is think of it like this firwst go left to right
+
+multiplying all numbers and storing in res
+
+then go righ to left multiplying the post fix sotring it in res and icnreasing it.
+
+*/
+const productExceptSelf = (nums) => {
+
+    let res = []
+
+    for (let i of nums){
+        res.push(1)
+    }
+
+    let prefix = 1;
+
+    for (let i=0; i<nums.length;i++){
+        res[i] = prefix;
+        prefix *= nums[i];
+    }
+
+
+    let postfix = 1;
+
+    for(let i= nums.length - 1; i>-1; i--){
+        res[i] *= postfix;
+        postfix *= nums[i];
+    }
+
+    return res
+
+}
